61fe07a8619243aadf85809ef457ec62c0e51615
Ralph Bean <rbean@redhat.com>
2014-02-07 12:00:52 -0500

    Logging and simplification.

--- fedmsg.d/example-config.py
+++ fedmsg.d/example-config.py
@@ -1,3 +1,14 @@
+import socket
+hostname = socket.gethostname()
+
 config = {
+    # This is just a key to tell the fedmsg-hub to initialize us.
     'summershum.enabled': True,
+
+    # Here is where we will publish our status
+    'endpoints': {
+        'summershum.%s' % hostname: [
+            "tcp://127.0.0.1:99887",
+        ],
+    },
 }
--- summershum/consumer.py
+++ summershum/consumer.py
@@ -1,28 +1,33 @@
 import fedmsg.consumers
 import summershum.utils
 
+
 class SummerShumConsumer(fedmsg.consumers.FedmsgConsumer):
-    topic = 'org.fedoraproject.prod.git.lookaside.new'
-    config_key = 'summershum'
+    topic = 'org.fedoraproject.prod.*'
+    config_key = 'summershum.enabled'
 
     def consume(self, msg):
+        msg = msg['body']['msg']
+        self.log.info("Ingesting %r" % msg.get('filename'))
         fedmsg.publish(
             topic='ingest.start',
-            msg=dict(original=message['msg']),
+            msg=dict(original=msg),
         )
         try:
-            summershum.utils.download_lookaside(message['msg'])
-            summershum.utils.get_sha1sum(message['msg'])
+            summershum.utils.download_lookaside(msg)
+            summershum.utils.get_sha1sum(msg)
         except Exception as e:
+            self.log.error("Failed to ingest %r %r" % (msg.get('filename'), e))
             fedmsg.publish(
                 topic='ingest.fail',
                 msg=dict(
-                    original=message['msg'],
+                    original=msg,
                     error=str(e),
                 ),
             )
         else:
+            self.log.info("Done ingesting %r" % msg.get('filename'))
             fedmsg.publish(
                 topic='ingest.complete',
-                msg=dict(original=message['msg']),
+                msg=dict(original=msg),
             )
