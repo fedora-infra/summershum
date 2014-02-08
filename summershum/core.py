488efaae55918c6be4e515767252defde94b0630
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-02-09 00:33:59 +0100

    Fixup indentation in the core file

--- summershum/core.py
+++ summershum/core.py
@@ -7,35 +7,35 @@ log = logging.getLogger("summershum")
 
 
 def ingest(session, msg, config, force=False):
-        if summershum.model.Package.by_pkg_sum(session, msg['md5sum']) \
-                and not force:
-            log.info("Skipping %r - md5sum already found in the database", msg.get('filename'))
-            return
-        log.info("Ingesting %r" % msg.get('filename'))
+    if summershum.model.Package.by_pkg_sum(session, msg['md5sum']) \
+            and not force:
+        log.info("Skipping %r - md5sum already found in the database", msg.get('filename'))
+        return
+    log.info("Ingesting %r" % msg.get('filename'))
+    fedmsg.publish(
+        topic='ingest.start',
+        msg=dict(original=msg),
+    )
+
+    lookaside_url = config['summershum.lookaside']
+    tmpdir = config['summershum.tmpdir']
+
+    try:
+        summershum.utils.download_lookaside(msg, lookaside_url, tmpdir)
+        summershum.utils.get_sha1sum(session, msg, tmpdir)
+    except Exception as e:
+        log.exception(e)
+        log.error("Failed to ingest %r %r" % (msg.get('filename'), e))
         fedmsg.publish(
-            topic='ingest.start',
+            topic='ingest.fail',
+            msg=dict(
+                original=msg,
+                error=str(e),
+            ),
+        )
+    else:
+        log.info("Done ingesting %r" % msg.get('filename'))
+        fedmsg.publish(
+            topic='ingest.complete',
             msg=dict(original=msg),
         )
-
-        lookaside_url = config['summershum.lookaside']
-        tmpdir = config['summershum.tmpdir']
-
-        try:
-            summershum.utils.download_lookaside(msg, lookaside_url, tmpdir)
-            summershum.utils.get_sha1sum(session, msg, tmpdir)
-        except Exception as e:
-            log.exception(e)
-            log.error("Failed to ingest %r %r" % (msg.get('filename'), e))
-            fedmsg.publish(
-                topic='ingest.fail',
-                msg=dict(
-                    original=msg,
-                    error=str(e),
-                ),
-            )
-        else:
-            log.info("Done ingesting %r" % msg.get('filename'))
-            fedmsg.publish(
-                topic='ingest.complete',
-                msg=dict(original=msg),
-            )
