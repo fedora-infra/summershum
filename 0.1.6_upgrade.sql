6c3f08ab4a8864c42fc4df295e828c7fca9da297
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-11-28 13:47:47 +0100

    Adjust the SQL script to update the current model to the new schema

--- 0.1.6_upgrade.sql
+++ 0.1.6_upgrade.sql
@@ -1,5 +1,13 @@
-INSERT INTO packages VALUES (SELECT DISTINCT pkg_name FROM files);
+INSERT INTO packages (pkg_name) VALUES (SELECT DISTINCT pkg_name FROM files);
 
-ALTER TABLE files
-ADD CONSTRAINT fk_pkg FOREIGN KEY (pkg_name)
+INSERT INTO releases (pkg_name, tarball, tar_sum) VALUES (
+    SELECT DISTINCT pkg_name, tar_file, tar_sum FROM files
+);
+
+ALTER TABLE files DROP COLUMN pkg_name, tar_file, tar_sum;
+
+ALTER TABLE releases ADD CONSTRAINT fk_pkg FOREIGN KEY (pkg_name)
     REFERENCES packages(pkg_name);
+
+ALTER TABLE files ADD CONSTRAINT fk_pkg FOREIGN KEY (tarball)
+    REFERENCES releases(tarball);
