2573484fa7d7af7898622fbd357e75d83390be6f
charul <charul.agrl@gmail.com>
2015-03-27 00:14:54 +0530

    Correct indentation

--- tests/test_sqlalchemy.py
+++ tests/test_sqlalchemy.py
@@ -14,14 +14,15 @@ class TestQuery(unittest.TestCase):
 
         Base.metadata.create_all(self.engine)
         self.file = File(
-	    filename='/pcre-8.36/makevp_l.txt', 
-            sha256sum=adeb5f421a231c3191246501e48f0306506a90fee4cebfe61db4d24e18df81f8, 
-	    sha1sum=fac84e09fcf69ae985c81f2f93f72281a693a05a, 
-	    md5sum=8891f3a734f71a632c8b43d48e88b336, 
-	    pkg_name='mingw-pcre', 
-	    tarball=pcre-8.36.tar.bz2, 
-	    tar_sum=b767bc9af0c20bc9c1fe403b0d41ad97, 
-	    created_on=datetime('2014-12-25 21:49:44.176689')
+            filename='/pcre-8.36/makevp_l.txt',
+            sha256sum=adeb5f421a231c3191246501e48f0306506a90fee4cebfe61db4d24e18df81f8,
+            sha1sum=fac84e09fcf69ae985c81f2f93f72281a693a05a,
+            md5sum=8891f3a734f71a632c8b43d48e88b336,
+            pkg_name='mingw-pcre',
+            tarball=pcre-8.36.tar.bz2,
+            tar_sum=b767bc9af0c20bc9c1fe403b0d41ad97,
+            created_on=datetime('2014-12-25 21:49:44.176689')
+            )
         self.session.add(self.file)
         self.session.commit()
 
@@ -32,12 +33,13 @@ class TestQuery(unittest.TestCase):
         expected = [self.file]
         result = self.session.query(File).all()
         expected =  File(
-	    filename='/pcre-8.36/makevp_l.txt', 
-            sha256sum=adeb5f421a231c3191246501e48f0306506a90fee4cebfe61db4d24e18df81f8, 
-	    sha1sum=fac84e09fcf69ae985c81f2f93f72281a693a05a, 
-	    md5sum=8891f3a734f71a632c8b43d48e88b336, 
-	    pkg_name='mingw-pcre', 
-	    tarball=pcre-8.36.tar.bz2, 
-	    tar_sum=b767bc9af0c20bc9c1fe403b0d41ad97, 
-	    created_on=datetime('2014-12-25 21:49:44.176689')
+            filename='/pcre-8.36/makevp_l.txt',
+            sha256sum=adeb5f421a231c3191246501e48f0306506a90fee4cebfe61db4d24e18df81f8,
+            sha1sum=fac84e09fcf69ae985c81f2f93f72281a693a05a,
+            md5sum=8891f3a734f71a632c8b43d48e88b336,
+            pkg_name='mingw-pcre',
+            tarball=pcre-8.36.tar.bz2,
+            tar_sum=b767bc9af0c20bc9c1fe403b0d41ad97,
+            created_on=datetime('2014-12-25 21:49:44.176689')
+            )
         self.assertEqual(result, expected)
